8e9c1e76011245c9523009d69825d8fa91a5662d
Ben Therien <cs135cid@yahoo.com>
2019-03-17 16:49:35 -0400

    Create KeyPressInterrupt.ps1

---
+++ KeyPressInterrupt.ps1
@@ -0,0 +1,9 @@
+﻿#Pause Script if #ANY# key is pressed
+Write-Host ""
+Write-Host ""
+Write-Host "###################################" -ForegroundColor Red
+Write-Host "## Press Any Key to PAUSE SCRIPT ##" -ForegroundColor Red
+Write-Host "###################################" -ForegroundColor Red
+Start-Sleep -Milliseconds 500 #Gives the user the CHANCE to see this message
+
+IF ($host.name -eq 'ConsoleHost') {IF ([console]::KeyAvailable) {pause}}
